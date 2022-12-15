e2aa50cce7592a996b83e1e0e22022e9c4b0f995
matheusnff85 <math.marinho154@gmail.com>
2022-12-15 18:42:16 -0300

    feat: implementa teste para a classe de produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "God of War",
+        "Sony",
+        "16/11/2022",
+        "16/11/2025",
+        "GOWRAG2K22",
+        "instrução 1",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "God of War"
+    assert product.nome_da_empresa == "Sony"
+    assert product.data_de_fabricacao == "16/11/2022"
+    assert product.data_de_validade == "16/11/2025"
+    assert product.numero_de_serie == "GOWRAG2K22"
+    assert product.instrucoes_de_armazenamento == "instrução 1"
