5156230123db94a608d309157d5ee15992338ee4
matheusnff85 <math.marinho154@gmail.com>
2022-12-15 19:28:44 -0300

    feat: implementa função de simple report requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, product_list: list):
+        oldest_product = min(
+            [item["data_de_fabricacao"] for item in product_list]
+        )
+        closest_validity = min(
+            [
+                item["data_de_validade"]
+                for item in product_list
+                if item["data_de_validade"]
+                > datetime.now().strftime("%Y-%m-%d")
+            ]
+        )
+        company, _ = Counter(
+            item["nome_da_empresa"] for item in product_list
+        ).most_common()[0]
+        return (
+            f"Data de fabricação mais antiga: {oldest_product}\n"
+            f"Data de validade mais próxima: {closest_validity}\n"
+            f"Empresa com mais produtos: {company}"
+        )
