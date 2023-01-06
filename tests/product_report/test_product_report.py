c3ac7f2d9cfb43fdfbe4197b4d5dfb3fad70cf16
matheusnff85 <math.marinho154@gmail.com>
2023-01-06 12:05:11 -0300

    feat: teste product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Playstation 5",
+        "Amazon",
+        "16/11/2022",
+        "16/11/2023",
+        "123456789",
+        "Armazenar em um local com sombra",
+    )
+    assert ("Playstation 5" in produto.__repr__()) is True
+    assert ("Amazon" in produto.__repr__()) is True
+    assert ("16/11/2022" in produto.__repr__()) is True
+    assert ("16/11/2023" in produto.__repr__()) is True
+    assert ("Armazenar em um local com sombra" in produto.__repr__()) is True
